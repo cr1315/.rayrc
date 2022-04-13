f099f22ccd1ffe25253b332f68089dcc222efecb
taihang.lei <taihang.lei@accenture.com>
2022-04-13 13:00:57 +0900

    install vim plugin automatically

---
+++ linux/51_vim/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+command -v vim >/dev/null 2>&1 || { return; }
+
+
+__rayrc_vim_install() {
+	local __rayrc_vim_install_dir="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+	# echo "__rayrc_vim_install_dir: ${__rayrc_vim_install_dir}"
+
+    # we don't need a `.vimrc`
+    if [[ -f "$HOME/.vimrc" ]]; then
+        mv "$HOME/.vimrc" "$HOME/.rayrc_bkup.vimrc"
+    fi
+
+    ### download plug.vim
+    curl -fLo "$__rayrc_vim_install_dir/vimfiles/autoload/plug.vim" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    ### ln vimfiles to ~/.vim
+    # mkdir ./rayrc_backup; [[ -f ~/.vimrc ]] && cp -fp ~/.vimrc ./rayrc_backup
+    # determine & ln -snF
+    ln -snF "$__rayrc_vim_install_dir/vimfiles" ~/.vim
+
+
+    # vim -u "$__rayrc_vim_install_dir/vimfiles/plugins.vim" +PlugInstall +qa &> /dev/null
+    vim -u "$__rayrc_vim_install_dir/vimfiles/plugins.vim" +PlugInstall +qa
+    echo "###### after PlugInstall #####"
+    pwd
+    ls -ahl "$__rayrc_vim_install_dir/vimfiles"
+    ls -ahl "$__rayrc_vim_install_dir/vimfiles/plugged"
+
+}
+
+__rayrc_vim_install
+unset -f __rayrc_vim_install
+
+
--- linux/51_vim/main.sh
+++ linux/51_vim/main.sh
@@ -7,29 +7,7 @@ __rayrc_vim_setup() {
 	local __rayrc_vim_setup_dir="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 	# echo "__rayrc_vim_setup_dir: ${__rayrc_vim_setup_dir}"
 
-	if [[ -h "$HOME/.vim" || ! -d "$HOME/.vim" ]]; then
-        # if [[  && ! "$(readlink -f ~/.vim)" = *".rayrc"* ]]; then do stuff; fi
-        # echo "ln -snf"
-        ln -snf "${__rayrc_vim_setup_dir}/vimfiles" "$HOME/.vim"
 
-    # elif [[  ]]; then
-    #     echo "bind mount"
-    #     mkdir "$HOME/.vim"
-    #     mount -o bind "${__rayrc_vim_setup_dir}/vimfiles" "$HOME/.vim"
-
-    else
-        # it's annoyed..
-        # first check if it's a bind mount
-        #   if not override it with a `mount -o bind`
-        #   if it is, you won't like to do the same target `mount -o bind` multiple times
-        #     so check the target and decide then..
-        true
-    fi
-
-    # we don't need a `.vimrc`
-    if [[ -f "$HOME/.vimrc" ]]; then
-        mv "$HOME/.vimrc" "$HOME/.rayrc_bkup.vimrc"
-    fi
 
 }
 
