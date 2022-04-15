2bb8b7d3d6fa2d56c7a4a1a8b71732a92deb14c8
taihang.lei <taihang.lei@accenture.com>
2022-04-15 18:33:25 +0900

    save for test in github action

--- linux/install.sh
+++ linux/install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# actually, we even don't need this much
-# and as by times, we don't even have git installed, or we don't have Internet connected..
-
 # we would need to bring compiled bins to the workstation..
 # then it would be boring to prepare for the target platform? CPU type? etc..
 
@@ -10,27 +7,50 @@
 # at least, write beautiful code that would be worked as assumed!
 # coding is just a hard work! -> It's you who make it an artifact!
 
-
 # determine if there is git installed
+
 
 __rayrc_linux_install() {
 	# SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 	local __rayrc_linux_install_dir="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 	# echo "__rayrc_linux_install_dir: ${__rayrc_linux_install_dir}"
 
-    ### setup bashrc
-    cat <<EOF >> $HOME/.bashrc
+	#
+	# would be better to determine if this is an EC2 instance, photon OS, ubuntu, CentOS
+	#
+	# for EC2:
+	#   `sudo dmidecode --string system-uuid'
+	#   or `cat /sys/hypervisor/uuid'
+	# TODO: case switch
+
+
+	### auto setup
+	for dir in `ls -1 "${__rayrc_linux_install_dir}"`; do
+
+		# echo "\${__rayrc_linux_install_dir}/\${dir}: ${__rayrc_linux_install_dir}/${dir}"
+		if [[ -d "${__rayrc_linux_install_dir}/${dir}" && -f "${__rayrc_linux_install_dir}/${dir}/install.sh" ]]; then
+			source "${__rayrc_linux_install_dir}/${dir}/install.sh"
+		fi
+
+	done
+
+
+	### after all installation completed, setup the .bashrc
+	### can we assume grep installed?
+	if [[ -f "$HOME/.bashrc" ]]; then
+		if grep -q '.rayrc' "$HOME/.bashrc"; then
+			# we assume sed installed..
+			sed -i -e '/\.rayrc.*main\.sh/ d' "$HOME/bashrc"
+
+# use here document to add two lines
+cat <<EOF >> $HOME/.bashrc
 
 [[ -f "${__rayrc_linux_install_dir}/main.sh" ]] && source "${__rayrc_linux_install_dir}/main.sh"
 EOF
 
-	### auto setup
-	for dir in `ls -1 "${__rayrc_linux_install_dir}"`; do
-		# echo "\$__rayrc_linux_install_dir/\$dir: $__rayrc_linux_install_dir/$dir"
-		if [[ -d "$__rayrc_linux_install_dir/$dir" && -f "$__rayrc_linux_install_dir/$dir/install.sh" ]]; then
-			source "$__rayrc_linux_install_dir/$dir/install.sh"
 		fi
-	done
+	fi
+
 }
 
 
