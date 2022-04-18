3f0c24a2cb161da4830b7b1cc0846d401d5fc507
taihang.lei <taihang.lei@accenture.com>
2022-04-18 23:14:21 +0900

    boot with new main

--- bash/main.sh
+++ bash/main.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
 
-__rayrc_main() {
-	# SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
-	local __rayrc_main_dir="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-	# echo "__rayrc_main_dir: ${__rayrc_main_dir}"
-	if [[ ! "$PATH" == *"${__rayrc_main_dir}"/00_bin* ]]; then
-		export PATH="${__rayrc_main_dir}"/00_bin"${PATH:+:${PATH}}"
+
+__rayrc_delegate_main() {
+    local __rayrc_raypm
+    local __rayrc_dir_shell
+
+	local __rayrc_dir_bin
+
+    __rayrc_dir_shell="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+    # echo "\${__rayrc_dir_shell}: ${__rayrc_dir_shell}"
+
+
+    # determine the os type and set __rayrc_stat_os
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		__rayrc_stat_os="linux"
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		__rayrc_stat_os="macos"
+	else
+		echo ".rayrc: not supported OS by now.."
+		return 8
 	fi
 
+
+	#
+	# would be better to determine if this is an EC2 instance, photon OS, ubuntu, CentOS
+	#
+	# for EC2:
+	#   `sudo dmidecode --string system-uuid'
+	#   `cat /sys/hypervisor/uuid'
+	# TODO: case switch
+	#       set __rayrc_stat_os_dist
+
+
 	### auto setup
-	for dir in `ls -1 "${__rayrc_main_dir}"`; do
-		# echo "\$__rayrc_main_dir/\$dir: $__rayrc_main_dir/$dir"
-		if [[ -d "$__rayrc_main_dir/$dir" && -f "$__rayrc_main_dir/$dir/main.sh" ]]; then
-			source "$__rayrc_main_dir/$dir/main.sh"
+	for package in `ls -1 "${__rayrc_dir_shell}"`; do
+
+		# echo "\${__rayrc_dir_shell}/\${package}: ${__rayrc_dir_shell}/${package}"
+		if [[ -d "${__rayrc_dir_shell}/${package}" && -f "${__rayrc_dir_shell}/${package}/main.sh" ]]; then
+			source "${__rayrc_dir_shell}/${package}/main.sh"
 		fi
+
 	done
 }
 
 
-__rayrc_main
-unset -f __rayrc_main
+__rayrc_delegate_main
+unset -f __rayrc_delegate_main
