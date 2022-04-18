8c3a20e7289f23240f455a620ee2119afd230b7c
taihang.lei <taihang.lei@accenture.com>
2022-04-18 23:21:04 +0900

    fix the design bug for __rayrc_dir_libs

--- bash/main.sh
+++ bash/main.sh
@@ -5,11 +5,17 @@ __rayrc_delegate_main() {
     local __rayrc_raypm
     local __rayrc_dir_shell
 
+	local __rayrc_dir_base
+	local __rayrc_dir_libs
 	local __rayrc_dir_bin
 
     __rayrc_dir_shell="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
     # echo "\${__rayrc_dir_shell}: ${__rayrc_dir_shell}"
 
+	# actually, this was a bug...
+	# I couldn't calculate this much..
+	__rayrc_dir_base="$(cd -- "${__rayrc_dir_shell}/.." && pwd -P)"
+	__rayrc_dir_libs="$(cd -- "${__rayrc_dir_shell}/../libs" && pwd -P)"
 
     # determine the os type and set __rayrc_stat_os
 	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
