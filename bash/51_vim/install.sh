cfb9c846501ebfca8b5dc6d2a03176ee845694d3
taihang.lei <taihang.lei@accenture.com>
2022-04-18 20:05:02 +0900

    fix typo, used dir_data_git in vim installation

--- bash/51_vim/install.sh
+++ bash/51_vim/install.sh
@@ -14,33 +14,33 @@ __rayrc_install_vim() {
     __rayrc_dir_data_vim="${__rayrc_dir_libs}/${package:3}"
     # echo "\${__rayrc_dir_data_vim}: ${__rayrc_dir_data_vim}"
 
-    if [[ ! -d "${__rayrc_dir_data_git}/__rayrc_backup" ]]; then
-        mkdir -p "${__rayrc_dir_data_git}/__rayrc_backup"
+    if [[ ! -d "${__rayrc_dir_data_vim}/__rayrc_backup" ]]; then
+        mkdir -p "${__rayrc_dir_data_vim}/__rayrc_backup"
     fi
 
 
     # backup the user's .vimrc or even .vim folder
     if [[ -f "$HOME/.vimrc" ]]; then
-        mv "$HOME/.vimrc" "${__rayrc_dir_data_git}/__rayrc_backup/.vimrc"
+        mv "$HOME/.vimrc" "${__rayrc_dir_data_vim}/__rayrc_backup/.vimrc"
     fi
     if [[ -d "$HOME/.vim" && ! -h "$HOME/.vim" ]]; then
-        mv "$HOME/.vim" "${__rayrc_dir_data_git}/__rayrc_backup/.vim"
+        mv "$HOME/.vim" "${__rayrc_dir_data_vim}/__rayrc_backup/.vim"
     fi
 
     ### download plug.vim
     # __rayrc_url_downloader https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
-    #    "${__rayrc_dir_data_git}/vimfiles/autoload/plug.vim"
-    curl -fLo "${__rayrc_dir_data_git}/vimfiles/autoload/plug.vim" --create-dirs \
+    #    "${__rayrc_dir_data_vim}/vimfiles/autoload/plug.vim"
+    curl -fLo "${__rayrc_dir_data_vim}/vimfiles/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     ### ln our vimfiles to ~/.vim
-    ln -snf "${__rayrc_dir_data_git}/vimfiles" ~/.vim
+    ln -snf "${__rayrc_dir_data_vim}/vimfiles" ~/.vim
 
-    vim -u "${__rayrc_dir_data_git}/vimfiles/plugins.vim" +PlugInstall +qa >& /dev/null
+    vim -u "${__rayrc_dir_data_vim}/vimfiles/plugins.vim" +PlugInstall +qa >& /dev/null
     echo "###### after PlugInstall #####"
     pwd
-    ls -ahl "${__rayrc_dir_data_git}/vimfiles"
-    ls -ahl "${__rayrc_dir_data_git}/vimfiles/plugged"
+    ls -ahl "${__rayrc_dir_data_vim}/vimfiles"
+    ls -ahl "${__rayrc_dir_data_vim}/vimfiles/plugged"
 
 }
 
