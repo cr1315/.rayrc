cdc10f95e3be72c403e0d030bbbb710eb7b1d6de
taihang.lei <taihang.lei@accenture.com>
2022-04-25 21:23:09 +0900

    test for __rayrc_stat_os

--- bash/06_rg/install.sh
+++ bash/06_rg/install.sh
@@ -13,6 +13,8 @@ __rayrc_install_rg() {
     # echo "\${__rayrc_dir_data_rg}: ${__rayrc_dir_data_rg}"
     [[ ! -d "${__rayrc_dir_data_rg}" ]] && mkdir -p "${__rayrc_dir_data_rg}"
 
+    echo "${__rayrc_stat_os}"
+    [[ "${__rayrc_stat_os}" =~ "linux" ]] && echo "true"
     if [[ "${__rayrc_stat_os}" =~ "linux" ]]; then
         if `uname -m` | grep -q "86" >& /dev/null; then
             __rayrc_github_downloader \
@@ -28,6 +30,7 @@ __rayrc_install_rg() {
                 "linux" "arm"
         else
             echo ".rayrc: unsupported cpu architecture for downloading rg.."
+            return 8
         fi
     elif [[ "${__rayrc_stat_os}" =~ "macos" ]]; then
         if `uname -m` | grep -q "86" >& /dev/null; then
@@ -40,9 +43,11 @@ __rayrc_install_rg() {
                 "linux" "arm"
         else
             echo ".rayrc: unsupported cpu architecture for downloading rg.."
+            return 8
         fi
     else
         echo ".rayrc: unsupported os for downloading rg.."
+        return 8
     fi
 
     tar xf "${__rayrc_dir_data_rg}/rg.tar.gz" -C "${__rayrc_dir_data_rg}" --transform 's:^[^/]*:rg:'
