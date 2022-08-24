d099999319655f8d45cbab10dc8825ed07fc7f8d
RayOnAWX <584664105@qq.com>
2022-08-25 08:44:57 +0900

    install my fzf

--- bash/12_fzf/install.sh
+++ bash/12_fzf/install.sh
@@ -3,29 +3,21 @@
 __rayrc_install() {
     __rayrc_module_common_setup
 
-    if ! command -v fzf >&/dev/null; then
+    # echo "##### fzf not installed #####"
+    git clone --quiet --depth 1 https://github.com/junegunn/fzf.git \
+        "${__rayrc_data_dir}/fzf" >&/dev/null
 
-        # echo "##### fzf not installed #####"
-        git clone --quiet --depth 1 https://github.com/junegunn/fzf.git \
-            "${__rayrc_data_dir}/fzf" >&/dev/null
+    # we don't need to auto-generated configurations
+    "${__rayrc_data_dir}/fzf/install" --bin >&/dev/null
 
-        # we don't need to auto-generated configurations
-        "${__rayrc_data_dir}/fzf/install" --bin >&/dev/null
+    cp -f "${__rayrc_data_dir}"/fzf/bin/* "${__rayrc_bin_dir}"
 
-        cp -f "${__rayrc_data_dir}"/fzf/bin/* "${__rayrc_bin_dir}"
-
-        # echo ""
-        # echo "##### cat "${__rayrc_data_dir}/fzf/shell/completion.bash" #####"
-        # cat "${__rayrc_data_dir}/fzf/shell/completion.bash"
-        # echo ""
-        # echo "##### cat "${__rayrc_data_dir}/fzf/shell/key-bindings.bash" #####"
-        # cat "${__rayrc_data_dir}/fzf/shell/key-bindings.bash"
-
-    else
-        echo ""
-        echo "##### $(command -v fzf) installed #####"
-        echo ""
-    fi
+    # echo ""
+    # echo "##### cat "${__rayrc_data_dir}/fzf/shell/completion.bash" #####"
+    # cat "${__rayrc_data_dir}/fzf/shell/completion.bash"
+    # echo ""
+    # echo "##### cat "${__rayrc_data_dir}/fzf/shell/key-bindings.bash" #####"
+    # cat "${__rayrc_data_dir}/fzf/shell/key-bindings.bash"
 }
 
 __rayrc_install
