8de319b9b6482660a6a4df9122a8ed5df88643c4
rayOnCicdIac <taihang.lei@accenture.com>
2022-08-24 22:45:21 +0900

    clean up lf configs

--- bash/61_terraform/main.sh
+++ bash/61_terraform/main.sh
@@ -2,4 +2,17 @@
 
 command -v terraform >/dev/null 2>&1 || { return; }
 
-alias tf="terraform"
+__rayrc_main() {
+    __rayrc_module_common_setup
+
+    alias tf="terraform"
+
+    export TF_CLI_CONFIG_FILE="${__rayrc_data_dir}/.terraformrc"
+    export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+    if [[ ! -d "${TF_PLUGIN_CACHE_DIR}" ]]; then
+        mkdir -p "${TF_PLUGIN_CACHE_DIR}"
+    fi
+}
+
+__rayrc_main
+unset -f __rayrc_main
