6a3025a4e2ae311fff973349e641b868c1cd4601
Ray@ACN <taihang.lei@accenture.com>
2022-09-08 16:58:40 +0900

    fixed path for regedit remediation

--- powershell/80_regedit_remediation/fix_browser.ps1
+++ powershell/80_regedit_remediation/fix_browser.ps1
@@ -7,3 +7,9 @@ remove-Item HKLM:\Software\Policies\Google\Chrome\ExtensionInstallBlacklist
 
 remove-Item HKLM:\Software\Policies\Google\Chrome\ExtensionInstallSources
 remove-item hklm:\Software\Policies\Google\Chrome\SyncTypesListDisabled
+
+
+# https://docs.microsoft.com/en-us/deployedge/microsoft-edge-policies#extensions
+remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Edge\SyncTypesListDisabled
+remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallBlocklist
+
--- powershell/80_regedit_remediation/fix_edge.ps1
+++ powershell/80_regedit_remediation/fix_edge.ps1
@@ -2,3 +2,4 @@
 # https://docs.microsoft.com/en-us/deployedge/microsoft-edge-policies#extensions
 remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Edge\SyncTypesListDisabled
 remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallBlocklist
+
