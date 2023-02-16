8f1cd06521e3615a4f5e754a18523aeb2966b659
ray@iacworker <taihang.lei@accenture.com>
2023-02-16 18:08:44 +0900

    add TODO

--- bash/63_databricks/main.sh
+++ bash/63_databricks/main.sh
@@ -5,6 +5,7 @@ command -v databricks >/dev/null 2>&1 || { return; }
 __rayrc_main() {
     __rayrc_module_common_setup
 
+    ## TODO: parse .netrc and export databricks env vars
     if [[ -f "${HOME}/.databricksenv" ]]; then
         source "${HOME}/.databricksenv"
     fi
