39c3fed593df35d1e25c16e9c45aceb2f2e618cc
Ray@ACN <taihang.lei@accenture.com>
2023-02-17 15:43:34 +0900

    workable sshs on windows

--- powershell/10_powershell/main.ps1
+++ powershell/10_powershell/main.ps1
@@ -1,36 +1,5 @@
-# aliases
-sal ll ls
-sal grep sls
 
-
-# posh
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
-# $ThemeSettings.PromptSymbols.VirtualEnvSymbol = "ENV"
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+function sshs() {
+  $result = $(ls "$env:USERPROFILE/.ssh" -filter "*config" -recurse | %{sls '^Host' $_.fullname | %{$_.line -replace '^Host ','ssh '}} | fzf)
+  Invoke-Expression $result
 }
-
-
-# Work around an RS5/PSReadline-2.0.0+beta2 bug (Spacebar is not marked 'essential')
-Set-PSReadlineKeyHandler "Shift+SpaceBar" -ScriptBlock {
-        [Microsoft.Powershell.PSConsoleReadLine]::Insert(' ')
-}
-Import-Module PSReadLine
-
-
-# custom functions
-function iswfpack {
-  rm *.zip
-  $pwdName = (gi $pwd).name
-  ls -directory | %{Compress-Archive $_.name ($_.name+".zip")}
-  ls -file | Compress-Archive -dest ($pwdName+".zip")
-}
-
-function restart-anyconnect {
-  cat "C:\Users\taihang.lei\AppData\Local\Cisco\Cisco AnyConnect Secure Mobility Client\connect.txt" | & "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" '-s'
-}
-
