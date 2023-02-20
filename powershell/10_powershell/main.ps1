c89037ddbbefb4e539a8927273986e1bc6a8d61f
ray@iacworker <taihang.lei@accenture.com>
2023-02-20 11:24:45 +0900

    refactor powershell ssh

--- powershell/10_powershell/main.ps1
+++ powershell/10_powershell/main.ps1
@@ -1,5 +1,15 @@
 
-function sshs() {
-  $result = $(ls "$env:USERPROFILE/.ssh" -filter "*config" -recurse | %{sls '^Host' $_.fullname | %{$_.line -replace '^Host ','ssh '}} | fzf)
-  Invoke-Expression $result
+function ssh() {
+  $ssh_bin = (gcm ssh).path
+  # $args.count
+  if ($args.count -ge 2) {
+    $ssh_bin $args
+  }
+
+  $result = $(ls "$env:USERPROFILE/.ssh" -filter "*config" -recurse | %{
+    sls '^Host' $_.fullname | %{
+      $_.line -replace '^Host ','ssh '
+    }
+  } | fzf)
+  Invoke-Expression $($result -replace '^ssh', "${ssh_bin}")
 }
