b4126ac484746d81989e04aa3bcedef82684ad05
ray@iacworker <taihang.lei@accenture.com>
2023-02-21 15:00:17 +0900

    add ssh skeleton for zsh

---
+++ zsh/11_fzf/functions.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env zsh
+
+ssh() {
+    ## save ssh bin path for subclassing
+    local ssh_bin=`which ssh`
+
+    ## shortcut
+    if [[ "$#" -ge 2 ]]; then
+        # echo "parameters: $@"
+        $ssh_bin "$@"
+        return
+    fi
+    if [[ "$#" -eq 1 && "$1" =~ ^- ]]; then
+        # echo "parameters: $@"
+        $ssh_bin "$@"
+        return
+    fi
+
+    local search_pattern
+    local host_lists
+    local hosts_filtered
+    local hosts_filtered_count
+    local ssh_command
+
+    search_pattern="$1"
+    host_lists=$(find "${HOME}/.ssh" -name '*config' -exec sh -c "grep '^Host' {} | sed -E 's/^Host //'" \;)
+
+    if [[ ! "${search_pattern}" =~ ^[[:space:]]*$ ]]; then
+        hosts_filtered=$(echo "$host_lists" | grep -E "${search_pattern}")
+        # echo "hosts_filtered:" "$hosts_filtered"
+    else
+        hosts_filtered="$host_lists"
+    fi
+
+    hosts_filtered_count=$(echo "$hosts_filtered" | wc -l)
+    if [[ $hosts_filtered_count -gt 1 ]]; then
+        ssh_command=$(echo "$hosts_filtered" | sed -E "s|^|ssh |" | fzf --no-preview)
+    elif [[ $hosts_filtered_count -eq 1 ]]; then
+        ssh_command=$(echo "$hosts_filtered" | sed -E "s|^|ssh |")
+    else
+        ssh_command=$(echo "$host_lists" | sed -E "s|^|ssh |" | fzf --no-preview)
+    fi
+    ssh_command=$(echo "$ssh_command" | sed -E "s|^ssh|${ssh_bin}|")
+    ${ssh_command}
+}
