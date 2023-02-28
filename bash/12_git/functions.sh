a650ab58bbb4a7e9c9a70a4a6a2438da61cb561b
ray@iacworker <taihang.lei@accenture.com>
2023-02-28 23:28:09 +0900

    add git.user

---
+++ bash/12_git/functions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git.user() {
+    local email_type
+    email_type="$1"
+    if [[ $email_type =~ work ]]; then
+        git config --global user.email "taihang.lei@accenture.com"
+    else
+        git config --global user.email "584664105@qq.com"
+    fi
+    git config --global user.name "${USER}@$(hostname)"
+}
