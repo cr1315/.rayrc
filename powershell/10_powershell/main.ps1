a3567da40a28377889516f75dac768e6fa9f7b14
Ray@ACN <taihang.lei@accenture.com>
2023-03-07 17:56:43 +0900

    add ctrl+r for windows

--- powershell/10_powershell/main.ps1
+++ powershell/10_powershell/main.ps1
@@ -38,3 +38,22 @@ function ssh() {
   echo $($result -replace '^', "& '${ssh_bin}' ")
   Invoke-Expression $($result -replace '^', "& '${ssh_bin}' ")
 }
+
+# PowerShell Core7でもConsoleのデフォルトエンコーディングはsjisなので必要
+# [System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+# [System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+
+# git logなどのマルチバイト文字を表示させるため (絵文字含む)
+$env:LESSCHARSET = "utf-8"
+
+# 音を消す
+Set-PSReadlineOption -BellStyle None
+
+# 履歴検索
+Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock {
+    # いつからかawkの表示がされないようになったので、以下コマンドを実行
+    # Set-Alias awk C:\Users\saido\scoop\apps\msys2\current\usr\bin\awk.exe
+    ## $command = Get-Content (Get-PSReadlineOption).HistorySavePath | awk '!a[$0]++' | fzf --tac
+    $command = Get-Content (Get-PSReadlineOption).HistorySavePath | fzf --tac --no-sort
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($command)
+}
