a4b88446305b04982ea1b5e1a0f84a61e0373483
ray@iacworker <taihang.lei@accenture.com>
2023-04-29 04:22:28 +0900

    add window and title bar color gpo settings

---
+++ powershell/90_misc/03_gpo/window_title_bar_color.ps1
@@ -0,0 +1,5 @@
+## regedit
+名前	順序	アクション	ハイブ	キー	値の名前	種類	値のデータ
+AutoColorization	1	更新	HKEY_CURRENT_USER	Control Panel\Desktop	AutoColorization	REG_DWORD	00000001
+ColorPrevalence	2	更新	HKEY_CURRENT_USER	Software\Microsoft\Windows\DWM	ColorPrevalence	REG_DWORD	00000001
+ColorPrevalence	3	更新	HKEY_CURRENT_USER	Software\Microsoft\Windows\CurrentVersion\Themes\Personalize	ColorPrevalence	REG_DWORD	00000001
