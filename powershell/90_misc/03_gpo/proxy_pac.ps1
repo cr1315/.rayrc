3c14a61b24e97b7b223ac98ae72953f56248c1a8
Ray@ACN <taihang.lei@accenture.com>
2023-05-18 21:56:22 +0900

    add proxy_pac.reg

---
+++ powershell/90_misc/03_gpo/proxy_pac.ps1
@@ -0,0 +1,3 @@
+名前	順序	アクション	ハイブ	キー	値の名前	種類	値のデータ
+AutoDetect	4	更新	HKEY_CURRENT_USER	Software\Microsoft\Windows\CurrentVersion\Internet Settings	AutoDetect	REG_DWORD	00000000
+AutoConfigURL	5	更新	HKEY_CURRENT_USER	Software\Microsoft\Windows\CurrentVersion\Internet Settings	AutoConfigURL	REG_SZ	https://gitlab.semiauto.link/tlei/proxy/-/raw/main/proxy.pac
