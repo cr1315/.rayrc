cc44961ca26237f16944d0df6c3e8b656ec8b1d3
Ray@ACN <taihang.lei@accenture.com>
2025-05-20 14:56:53 +0900

    clear screen every startup

--- powershell/main.ps1
+++ powershell/main.ps1
@@ -3,9 +3,10 @@
 # Main
 #
 ######################################################################
-ls "${PSScriptRoot}" -directory | %{
+ls "${PSScriptRoot}" -directory | % {
     if (test-path "$($_.fullname)/main.ps1") {
         . "$($_.fullname)/main.ps1"
     }
 }
 
+cls
