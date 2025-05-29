b196569fb37201626b7dc116b2356346a52c217f
ray@nem-iacworker <taihang.lei@accenture.com>
2025-05-29 13:55:11 +0900

    fix pipx dependency relation

--- bash/20_pipx/install.sh
+++ bash/20_pipx/install.sh
@@ -6,29 +6,45 @@ __rayrc_install() {
     __rayrc_module_common_setup
 
     if ! command -v pipx >&/dev/null; then
-        python3 -m venv "${__rayrc_data_dir}/boot"
+        python3 -m venv "${__rayrc_data_dir}/self"
         (
-            source "${__rayrc_data_dir}/boot/bin/activate"
+            source "${__rayrc_data_dir}/self/bin/activate"
             pip install --upgrade pip
             pip install pipx
         ) >&/dev/null
 
-        ln -snf "${__rayrc_data_dir}/boot/bin/pipx" "${__rayrc_bin_dir}/pipx"
-    fi
+        ln -snf "${__rayrc_data_dir}/self/bin/pipx" "${__rayrc_bin_dir}/pipx"
 
-    export PIPX_HOME="${__rayrc_data_dir}/run"
-    export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+        export PIPX_HOME="${__rayrc_data_dir}/run"
+        export PIPX_BIN_DIR="${__rayrc_bin_dir}"
 
-    if ! command -v poetry >&/dev/null; then
-        "${__rayrc_bin_dir}/pipx" install poetry
-    fi
+        if ! command -v poetry >&/dev/null; then
+            "${__rayrc_bin_dir}/pipx" install poetry
+        fi
 
-    if ! command -v glances >&/dev/null; then
-        "${__rayrc_bin_dir}/pipx" install glances
-    fi
+        if ! command -v glances >&/dev/null; then
+            "${__rayrc_bin_dir}/pipx" install glances
+        fi
 
-    if ! command -v ansible >&/dev/null; then
-        "${__rayrc_bin_dir}/pipx" install --include-deps --system-site-packages "ansible<2.10"
+        if ! command -v ansible >&/dev/null; then
+            "${__rayrc_bin_dir}/pipx" install --include-deps --system-site-packages "ansible<2.10"
+        fi
+
+    else
+        export PIPX_HOME="${__rayrc_data_dir}/run"
+        export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+
+        if ! command -v poetry >&/dev/null; then
+            pipx install poetry
+        fi
+
+        if ! command -v glances >&/dev/null; then
+            pipx install glances
+        fi
+
+        if ! command -v ansible >&/dev/null; then
+            pipx install --include-deps --system-site-packages "ansible<2.10"
+        fi
     fi
 }
 
