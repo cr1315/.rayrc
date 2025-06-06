c7b2d0e4db707858b3b84a7fa8a6475c22da572b
ray@nem-iacworker <taihang.lei@accenture.com>
2025-06-06 23:28:05 +0900

    use venv to install pipx if pipx not found and venv installed

--- bash/20_pipx/install.sh
+++ bash/20_pipx/install.sh
@@ -6,30 +6,32 @@ __rayrc_install() {
     __rayrc_module_common_setup
 
     if ! command -v pipx >&/dev/null; then
-        python3 -m venv "${__rayrc_data_dir}/self"
-        (
-            source "${__rayrc_data_dir}/self/bin/activate"
-            pip install --upgrade pip
-            pip install pipx
-        ) >&/dev/null
+        if python3 -m venv -h >&/dev/null; then
 
-        ln -snf "${__rayrc_data_dir}/self/bin/pipx" "${__rayrc_bin_dir}/pipx"
+            python3 -m venv "${__rayrc_data_dir}/self"
+            (
+                source "${__rayrc_data_dir}/self/bin/activate"
+                pip install --upgrade pip
+                pip install pipx
+            ) >&/dev/null
 
-        export PIPX_HOME="${__rayrc_data_dir}/run"
-        export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+            ln -snf "${__rayrc_data_dir}/self/bin/pipx" "${__rayrc_bin_dir}/pipx"
 
-        if ! command -v poetry >&/dev/null; then
-            "${__rayrc_bin_dir}/pipx" install poetry
+            export PIPX_HOME="${__rayrc_data_dir}/run"
+            export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+
+            if ! command -v poetry >&/dev/null; then
+                "${__rayrc_bin_dir}/pipx" install poetry
+            fi
+
+            if ! command -v glances >&/dev/null; then
+                "${__rayrc_bin_dir}/pipx" install glances
+            fi
+
+            if ! command -v ansible >&/dev/null; then
+                "${__rayrc_bin_dir}/pipx" install --include-deps --system-site-packages "ansible<2.10"
+            fi
         fi
-
-        if ! command -v glances >&/dev/null; then
-            "${__rayrc_bin_dir}/pipx" install glances
-        fi
-
-        if ! command -v ansible >&/dev/null; then
-            "${__rayrc_bin_dir}/pipx" install --include-deps --system-site-packages "ansible<2.10"
-        fi
-
     else
         export PIPX_HOME="${__rayrc_data_dir}/run"
         export PIPX_BIN_DIR="${__rayrc_bin_dir}"
--- bash/20_pipx/main.sh
+++ bash/20_pipx/main.sh
@@ -5,14 +5,19 @@ command -v pipx >/dev/null 2>&1 || { return; }
 __rayrc_main() {
     __rayrc_module_common_setup
 
-    export PIPX_HOME="${__rayrc_data_dir}/run"
-    export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+    local pipx_full_path="$(realpath `which pipx`)"
 
-    local pipx_full_path="$(readlink `which pipx`)"
-    local pipx_complete_path="$(dirname $pipx_full_path)/register-python-argcomplete"
-    if [[ -f "${pipx_complete_path}" ]]; then
-        eval "$(${pipx_complete_path} pipx)"
-    fi
+    # if [[ ! "${pipx_full_path}" =~ ^[[:space:]]*$ ]]; then
+
+        export PIPX_HOME="${__rayrc_data_dir}/run"
+        export PIPX_BIN_DIR="${__rayrc_bin_dir}"
+
+        local pipx_complete_path="$(dirname $pipx_full_path)/register-python-argcomplete"
+        if [[ -f "${pipx_complete_path}" ]]; then
+            eval "$(${pipx_complete_path} pipx)"
+        fi
+
+    # fi
 }
 
 __rayrc_main
