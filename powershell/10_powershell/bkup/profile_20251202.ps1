d5656508b0d246029ee67234bf41901d72570963
Ray@ACN <taihang.lei@accenture.com>
2025-12-02 20:31:00 +0900

    simplify $PROFILE

--- powershell/10_powershell/bkup/profile_20251202.ps1
+++ powershell/10_powershell/bkup/profile_20251202.ps1
@@ -17,21 +17,6 @@ sal grep sls
 
 
 ######################################################################
-## KDDI custom functions
-######################################################################
-# function iswfpack {
-#   rm *.zip
-#   $pwdName = (gi $pwd).name
-#   ls -directory | %{Compress-Archive $_.name ($_.name+".zip")}
-#   ls -file | Compress-Archive -dest ($pwdName+".zip")
-# }
-
-# function restart-anyconnect {
-#   cat "C:\Users\taihang.lei\AppData\Local\Cisco\Cisco AnyConnect Secure Mobility Client\connect.txt" | & "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" '-s'
-# }
-
-
-######################################################################
 ## Chocolatey Profile
 ######################################################################
 # $ThemeSettings.PromptSymbols.VirtualEnvSymbol = "ENV"
