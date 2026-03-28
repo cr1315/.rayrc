0b9f3f701fe105296ad5d73210f1012c84305475
ray@nem-iacworker <taihang.lei@accenture.com>
2026-03-28 23:54:51 +0900

    use bedrock claude

---
+++ bash/70_claude/main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+command -v claude >/dev/null 2>&1 || { return; }
+
+__rayrc_main() {
+    __rayrc_module_common_setup
+
+    if command -v aws >/dev/null 2>&1 && \
+       aws bedrock list-foundation-models --region ap-northeast-1 >&/dev/null; then
+        export CLAUDE_CODE_USE_BEDROCK=1
+        export AWS_REGION=ap-northeast-1
+    fi
+
+}
+
+__rayrc_main
+unset -f __rayrc_main
